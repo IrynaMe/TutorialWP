67de33714ea4b63706ff13cf8ab6ebb81ed4ed64
IrynaMe <irinam7a@gmail.com>
2022-12-07 09:36:26 +0100

    plugin: -collect data -insert in db

--- wp-content/plugins/Form/Form.php
+++ wp-content/plugins/Form/Form.php
@@ -11,8 +11,17 @@ Author: Matt Mullenweg
 Version: 1.0
 Author URI: http://cfta.it
 */
-
+if ($_POST){
+    $email=$_POST['email'];
+    global $wpdb;
+    //$table_name='wp_email';
+    $table_name=$wpdb->prefix.'email';
+    $date=date('Y-m-d H:i:s');
+    //insert in DB using wp function wpdb, preparing query in wp-myadmin db
+    $wpdb->query("INSERT INTO {$table_name}( `email`, `inserito`) VALUES ('$email','$date')");
+}
 include_once('shortcode.php');
+
 
 
 
